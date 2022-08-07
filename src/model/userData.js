6760e8cd9d7a56bbf05c6205da6b6e5eb0337ca7
nivedmundiyath <102532951+nivedmundiyath@users.noreply.github.com>
2022-08-07 16:22:22 +0530

    commit

---
+++ src/model/userData.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:manage@clusterlibrary.wgitnvq.mongodb.net/clusterlibrary?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// mongoose.connect('mongodb://localhost:27017/LibraryBook');
+
+const Schema = mongoose.Schema;
+
+var NewUserSchema = new Schema({
+
+    username : String,
+    password :String
+});
+
+var Userdata = mongoose.model('user', NewUserSchema);
+
+module.exports = Userdata;
